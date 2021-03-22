9a0cbef3afd7eebc4e97d5d51fb60fdfe9308031
SreenivasSreee <ksreenivascse@gmail.com>
2021-03-22 23:59:15 +0530

    counter mechanism implemented

--- Egg planner/ViewController.swift
+++ Egg planner/ViewController.swift
@@ -8,14 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var hardness = ["Soft":5,"Medium":10,"Hard":15]
-
+    var hardness = ["Soft":5,"Medium":10*60,"Hard":15*60]
+    var counter = 0
+    var timer=Timer()
     @IBAction func softNess(_ sender: UIButton) {
+        timer.invalidate()
         var ness = sender.currentTitle!
-        print(hardness[ness]!)
+         counter = hardness[ness]!
+        
+         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
     }
     
-
+@objc func updateCounter() {
+    //example functionality
+    if counter > 0 {
+        print("\(counter) seconds ")
+        counter -= 1
+        if(counter==0){
+            print("Egg Ready")
+        }
+    }
+}
 
 }
 
