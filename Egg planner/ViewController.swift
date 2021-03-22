100e35cb94d2d69a8e52110fb16aa50364f351c0
SreenivasSreee <ksreenivascse@gmail.com>
2021-03-22 23:08:40 +0530

    Initial Commit

---
+++ Egg planner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Egg planner
+//
+//  Created by Sreenivas k on 22/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Egg plannerTests/Egg_plannerTests.swift
@@ -0,0 +1,33 @@
+//
+//  Egg_plannerTests.swift
+//  Egg plannerTests
+//
+//  Created by Sreenivas k on 22/03/21.
+//
+
+import XCTest
+@testable import Egg_planner
+
+class Egg_plannerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
