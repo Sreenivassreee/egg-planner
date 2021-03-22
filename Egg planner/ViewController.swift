a7956ecde5133d3aa4799d432e77fd737df60a19
SreenivasSreee <ksreenivascse@gmail.com>
2021-03-23 01:09:13 +0530

    finished application

--- Egg planner/ViewController.swift
+++ Egg planner/ViewController.swift
@@ -8,25 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var hardness = ["Soft":5,"Medium":10*60,"Hard":15*60]
-    var counter = 0
+   
+   
+    @IBOutlet weak var titleLable: UITextView!
+    
+    @IBOutlet weak var ProgressBar: UIProgressView!
+    var hardness = ["Soft":1*60,"Medium":10*60,"Hard":15*60]
+    var totalTime=0
+    var passedTime=0
+    
+    
     var timer=Timer()
     @IBAction func softNess(_ sender: UIButton) {
+        
         timer.invalidate()
-        var ness = sender.currentTitle!
-         counter = hardness[ness]!
+        ProgressBar.progress=0.0
+         passedTime=0
+        let ness = sender.currentTitle!
+        titleLable.text=ness
+         totalTime = hardness[ness]!
+    
         
          timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
     }
     
 @objc func updateCounter() {
-    //example functionality
-    if counter > 0 {
-        print("\(counter) seconds ")
-        counter -= 1
-        if(counter==0){
-            print("Egg Ready")
-        }
+    if passedTime < totalTime {
+        passedTime += 1
+        let progressTime = Float(passedTime)/Float(totalTime)
+        ProgressBar.progress=progressTime
+       
+    }else{
+        timer.invalidate()
+        titleLable.text="Done !"
     }
 }
 
